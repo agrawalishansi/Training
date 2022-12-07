2eecbc37f0787e7177539a2bf8a093c45d6b2a7e
ishansi-abjima <119054363+ishansi-abjima@users.noreply.github.com>
2022-12-07 15:02:25 +0530

    Add files via upload

---
+++ todaycode/todaycode/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using todaycode;
+
+namespace todaycode
+{
+    class LongestSubstringWithoutRepeatingCharacters
+    {
+        static void Main(string[] args)
+        {
+            int length = 0;
+            string s = "a";
+            for (int start = 0; start < s.Length; start++)
+            {
+                int i = start;
+                var dictionary = new Dictionary<char, int>();
+                while (i < s.Length && !dictionary.ContainsKey(s[i]))
+                {
+                    dictionary.Add(s[i++], 1);
+                }
+                if (i - start > length)
+                {
+                    length = i - start;
+                }
+            }
+            Console.WriteLine(length);
+        }
+
+    }
+ }
