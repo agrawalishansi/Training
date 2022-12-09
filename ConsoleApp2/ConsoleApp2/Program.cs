5e0bd70490f40533be391181a25ab2e271956d99
ishansi-abjima <119054363+ishansi-abjima@users.noreply.github.com>
2022-12-09 11:03:09 +0530

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ConsoleApp2;
+// program to multiply two strings 
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        string applesInBasket = "16";
+        string baskets = "4";
+        int total = Convert.ToInt32(applesInBasket) * Convert.ToInt32(baskets);
+        string msg = $"There are total of {total} apples";
+        Console.WriteLine(msg);
+
+
+    }
+}
