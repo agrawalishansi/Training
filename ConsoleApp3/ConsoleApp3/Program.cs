16792474412488477b93a7ad3d358d0a8a73104a
ishansi-abjima <119054363+ishansi-abjima@users.noreply.github.com>
2022-12-09 16:55:27 +0530

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ConsoleApp3;
+public class Program
+{
+    //To find all the subsets of a string
+    static void subString(String str)
+
+    {
+        int n = str.Length;
+        string[] array= new string[n];
+        for (int i = 0; i < n; i++) //To select the starting index
+        {
+            array[i] = str.Substring(0, i + 1);
+                Console.WriteLine(array[i]);
+            
+        }
+    }
+    // Driver program to test above function
+    static public void Main()
+    {
+        String str = "Hello";
+        Console.WriteLine("The entered string are " + str);
+        Console.WriteLine("All the substrings of the above string is: ");
+        //Call to find the all the subsets of the string
+        subString(str);
+    }
+}
---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace ConsoleApp4;
+public class Program
+{
+    //To find all the subsets of a string
+    static void subString(String str)
+
+    {
+        int n = str.Length;
+
+        for (int i = 0; i < n; i++) //To select the starting index
+        {
+            for (int j = 0; j <= n - i; j++) //To select the ending index
+            {
+                Console.WriteLine(str.Substring(i, j));
+            }
+        }
+    }
+    // Driver program to test above function
+    static public void Main()
+    {
+        String str = "Hello";
+        Console.WriteLine("The entered string are " + str);
+        Console.WriteLine("All the substrings of the above string is: ");
+        //Call to find the all the subsets of the string
+        subString(str);
+    }
+}
