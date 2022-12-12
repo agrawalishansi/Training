ae1c4198693b4f9b988d903ded091d98e28218de
ishansi-abjima <119054363+ishansi-abjima@users.noreply.github.com>
2022-12-12 10:18:38 +0530

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ConsoleApp1
+{
+    // reverse words in a string 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the String:");
+            
+            string s = Console.ReadLine();
+            string[] a = s.Split(' ');
+            Array.Reverse(a);
+            
+            Console.WriteLine("Reverse String is:");
+            for (int i = 0; i <= a.Length - 1; i++)
+            {
+                
+                Console.Write(a[i] + "" + ' ');
+            }
+            Console.ReadKey();
+        }
+    }
+}
