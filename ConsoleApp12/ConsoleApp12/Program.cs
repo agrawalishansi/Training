141a268c0df0a207a548dfb0d3117e4936dd031e
ishansi-abjima <119054363+ishansi-abjima@users.noreply.github.com>
2022-12-13 18:25:51 +0530

    Add files via upload

---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ConsoleApp12;
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        var sum = 0;
+        int[] array1 = { 1, 3, 15, 45, 9 };
+        int n = array1.Length;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (array1[i] % 3 == 0 && array1[i] % 5 == 0)
+            {
+                sum += array1[i];
+            }
+            Console.WriteLine(sum);
+
+        }
+
+        
+
+//        List<int> myList = new List<int>() {
+//   1,
+//   3,
+//            15,
+//            45,
+//};
+//        int n = myList.Count;
+//        for (int i = 0; i < n; i++)
+//        {
+//            if (myList[i] % 3 == 0 && myList[i] % 5 == 0)
+//            {
+//                sum += myList[i];
+//            }
+//        }
+
+//        Console.WriteLine(sum);
+
+    }
+}
