020162f151b9fd0880b261689f095a1a1a3972bc
ishansi-abjima <119054363+ishansi-abjima@users.noreply.github.com>
2022-12-28 17:59:09 +0530

    Add files via upload

---
+++ ConsoleApp41/ConsoleApp41/Program4.cs
@@ -0,0 +1,39 @@
+﻿
+//using ConsoleApp41;
+//using System;
+
+//namespace ConsoleApp41
+//{
+//    public class Program4
+//{
+//    public static void Main(string[] args)
+//    {
+
+//        Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+//        int amount = 1234;
+//        int months = 12;
+//        int bonus = 1000;
+
+//        int yearlyWage = Utilities.CalculateYearlyWage(amount, months);
+//        int yearlyWageWithBonus = Utilities.CalculateYearlyWage(amount, months, bonus);
+
+//        Console.WriteLine($"Yearly wage: {yearlyWage}");
+
+
+//        double amountDouble = 1500.0;
+//        double monthsDouble = 12;
+//        double bonusDouble = 1000;
+
+//        double yearlyWageWithBonusDouble = Utilities.CalculateYearlyWage(amountDouble, monthsDouble, bonusDouble);
+
+//        Utilities.UsingOptionalParameters();
+
+//        Utilities.UsingNamedArguments();
+
+//        Console.ReadLine();
+
+
+//    }
+//}
+//}
+
---
+++ ConsoleApp41/ConsoleApp41/Program6.cs
@@ -0,0 +1,50 @@
+﻿
+using System;
+
+namespace ConsoleApp41
+{
+
+    public class Program6
+    {
+
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+
+
+            int amount = 1234;
+            int months = 12;
+            int bonus = 1000;
+
+            int yearlyWage = Utilities2.CalculateYearlyWage(amount, months);
+            int yearlyWageWithBonus = Utilities2.CalculateYearlyWage(amount, months, bonus);
+
+            Console.WriteLine($"Yearly wage: {yearlyWage}");
+
+
+            double amountDouble = 1500.0;
+            double monthsDouble = 12;
+            double bonusDouble = 1000;
+
+            double yearlyWageWithBonusDouble = Utilities2.CalculateYearlyWage(amountDouble, monthsDouble, bonusDouble);
+
+            Utilities2.UsingOptionalParameters();
+
+            Utilities2.UsingNamedArguments();
+
+            Utilities2.ManipulatingStrings();
+
+            Utilities2.UsingEscapeCharacters();
+
+            Utilities2.UsingStringEquality();
+
+            Utilities2.ParsingStrings();
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
